90f4fbc8b09b96a09cbfb3fa312ce6db24453b56
Gil Lopes Bueno <gil.bueno@martinlabs.com.br>
2017-10-20 01:21:03 -0200

    PagedResp’s list non nullable

--- src/main/kotlin/com/simpli/model/PagedResp.kt
+++ src/main/kotlin/com/simpli/model/PagedResp.kt
@@ -1,11 +1,13 @@
 package com.simpli.model
 
+import java.util.*
+
 /**
  *
  * @author gil
  */
 class PagedResp<T> {
-    var list: List<T>? = null
+    var list: List<T> = LinkedList<T>()
     var recordsTotal: Int = 0
 
     constructor() {}
